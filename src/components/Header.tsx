76029e7517de28445b07744cb76017cb2a1c14ea
nayounsang <nayounsang722@gmail.com>
2023-12-09 20:01:36 +0900

    2nd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 
 const Header = () => {
-    return(
-        <div className = "header">
-            <h1>nayounsang1이 푼 백준 문제들</h1>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <h1>nayounsang1이 푼 백준 문제들</h1>
+      <h3
+        onClick={() => {
+          window.open(
+            "https://github.com/nayounsang/bojInfiniteScroll",
+            "_blank"
+          );
+        }}
+      >
+        깃허브 레포지토리
+      </h3>
+    </div>
+  );
+};
 
 export default Header;
